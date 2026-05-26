cdbc6fcff005a004e4f5491adb16a09df80dc281
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:10:35 +0100

    feat: implement tf-atom-db-parameter-group-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "id" {
+  description = "ID of the DB parameter group"
+  value       = try(aws_db_parameter_group.this[0].id, null)
+}
+
+output "name" {
+  description = "Name of the DB parameter group"
+  value       = try(aws_db_parameter_group.this[0].name, null)
+}
+
+output "arn" {
+  description = "ARN of the DB parameter group"
+  value       = try(aws_db_parameter_group.this[0].arn, null)
 }
